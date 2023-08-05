e9d172a8f668a0f462bc0906a62aa76b91eed36c
yucihent <yucihent@163.com>
2023-08-05 22:32:38 +0800

    chore(package): :hammer: modify package json name filed

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,6 @@ import frontmatter from 'remark-frontmatter'
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  // output: 'export',
-  // basePath: '/next-page',
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
   // experimental: {
   //   mdxRs: true
