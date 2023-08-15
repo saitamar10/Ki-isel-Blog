87922ac4389e677437fd1b1bb445d300718a1951
yucihent <yucihent@163.com>
2023-08-15 23:04:56 +0800

    chore: :hammer: weekly整理中

--- app/weekly/page.tsx
+++ app/weekly/page.tsx
@@ -4,26 +4,25 @@ import { fetchWeeklyList } from 'lib/fetch'
 export default async function Weekly() {
   const weekly = await fetchWeeklyList().catch(() => [])
 
-  return weekly.length ? (
-    <ul>
-      {weekly.map(({ slug, title }, index: number) => (
-        <li
-          key={slug}
-          className="prose prose-custom dark:prose-invert max-w-none mb-1"
-        >
-          <span className="mr-3">
-            #{index + 1} <time>{slug}</time>
-          </span>
-          <Link
-            href={`/weekly/${slug}`}
-            className="hover:text-sky-500 hover:underline dark:text-slate-300 dark:hover:text-sky-500"
-          >
-            <span>{title}</span>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  ) : (
-    <p>整理中...🔥</p>
+  return (
+    <h2>整理中...🔥</h2>
+    // <ul>
+    //   {weekly.map(({ slug, title }, index: number) => (
+    //     <li
+    //       key={slug}
+    //       className="prose prose-custom dark:prose-invert max-w-none mb-1"
+    //     >
+    //       <span className="mr-3">
+    //         #{index + 1} <time>{slug}</time>
+    //       </span>
+    //       <Link
+    //         href={`/weekly/${slug}`}
+    //         className="hover:text-sky-500 hover:underline dark:text-slate-300 dark:hover:text-sky-500"
+    //       >
+    //         <span>{title}</span>
+    //       </Link>
+    //     </li>
+    //   ))}
+    // </ul>
   )
 }
