0bac5e4cdd4cad3d024c83e46ea2e792fbbfa311
saitamar10 <94067189+saitamar10@users.noreply.github.com>
2023-08-17 19:53:39 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <p>
-        Hi there👋 我是赫子子, 欢迎来到我的空间👀, 在下方👇输入命令进行交互🥳🔥
+        Hi there👋Script Nasıl👀, Beğendin mi?👇İçeriklere bak🥳🔥
       </p>
       <CommandProvider versions={versions}>
         <CommandLine />
